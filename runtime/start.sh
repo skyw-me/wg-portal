f8bbb6269dfb0de8baa7909a9474a7512231ecaa
One <imone@tuta.io>
2022-03-05 14:26:34 +0800

    copy config on init

--- runtime/start.sh
+++ runtime/start.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+WG_CONFIG_PATH=/app/data/wireguard
+
+# copy initial config if not exists
+mkdir -p "$WG_CONFIG_PATH"
+cp -n /initial_config/* "$WG_CONFIG_PATH"
+
 # setup wireguard device
-wg-quick down ${WG_DEFAULT_DEVICE}
-wg-quick up ${WG_DEFAULT_DEVICE}
+WG_DEVICE_CONFIG_FILE="$WG_CONFIG_PATH/$WG_DEFAULT_DEVICE.conf"
+wg-quick down "$WG_DEVICE_CONFIG_FILE"
+wg-quick up "$WG_DEVICE_CONFIG_FILE"
 
 # main program
 if [ -n "${WAIT_FOR}" ]; then
